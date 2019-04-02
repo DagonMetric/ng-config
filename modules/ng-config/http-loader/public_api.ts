71b7bc18c40c3c79adaf9e484bbf3b4316e648f0
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 20:17:35 +0630

    Initial commit

---
+++ modules/ng-config/http-loader/public_api.ts
@@ -0,0 +1,3 @@
+export { CONFIG_ENDPOINT_URL } from './src/config-endpoint-url';
+export { ConfigHttpLoader } from './src/config-http-loader';
+export { ConfigHttpLoaderModule } from './src/config-http-loader.module';
