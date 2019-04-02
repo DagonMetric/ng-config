9feaba719ca7db7a4f9973a39d60106af906741e
Moe Myat Zaw <mmzliveid@gmail.com>
2019-04-02 20:18:26 +0630

    Initial commit

---
+++ modules/ng-config/src/index.ts
@@ -0,0 +1,6 @@
+export * from './config-loader-token';
+export * from './config-loader';
+export * from './config-static-loader.module';
+export * from './config-static-loader';
+export * from './config.module';
+export * from './config.service';
