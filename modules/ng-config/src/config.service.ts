db7ece9a104b3072041e20f91dc69062dceff3a8
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-02 15:47:06 +0630

    Rename to ConfigService

--- modules/ng-config/src/config.service.ts
+++ modules/ng-config/src/config.service.ts
@@ -6,41 +6,90 @@
  * found under the LICENSE file in the root directory of this source tree.
  */
 
-import { Inject, Injectable, InjectionToken, Optional } from '@angular/core';
+import { Inject, Injectable, Injector, Optional } from '@angular/core';
 
 import { BehaviorSubject, Observable, forkJoin, of } from 'rxjs';
 import { share, take, tap } from 'rxjs/operators';
 
 import { ConfigProvider } from './config-provider';
 import { CONFIG_PROVIDER } from './config-provider-token';
-import { ConfigTemplate } from './config-template';
 import { ConfigSection } from './config-section';
+import { CONFIG_OPTIONS, ConfigOptions } from './config-options';
 
 export interface ConfigLoadingContext {
     status?: 'loading' | 'loaded';
 }
 
-/**
- * Options for `ConfigService`.
- */
-export interface ConfigOptions {
-    /**
-     * Set true to log debug information.
-     */
-    trace?: boolean;
+export type NewableOptions<TOptions> = new () => TOptions;
+
+const OptionsSuffix = 'Options';
+
+interface OptionsBase {
+    [key: string]: string | number | boolean | OptionsBase | null;
 }
 
-export const CONFIG_OPTIONS = new InjectionToken<ConfigOptions>('ConfigOptions');
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+function mapOptionValues(options: OptionsBase, configSection: ConfigSection): void {
+    const keys = Object.keys(options);
+    for (const key of keys) {
+        if (!Object.prototype.hasOwnProperty.call(configSection, key)) {
+            continue;
+        }
+
+        const optionsValue = options[key];
+        const configValue = configSection[key];
+
+        if (optionsValue === configValue) {
+            continue;
+        }
+
+        if (configValue == null) {
+            options[key] = null;
+            continue;
+        }
+
+        if (optionsValue == null) {
+            options[key] = configValue;
+            continue;
+        }
+
+        if (typeof optionsValue === 'string') {
+            if (typeof configValue === 'string') {
+                options[key] = configValue;
+            } else if (typeof configValue === 'number') {
+                options[key] = (configValue as number).toString();
+            } else if (typeof configValue === 'boolean') {
+                options[key] = (configValue as boolean).toString();
+            } else {
+                options[key] = JSON.stringify(configValue);
+            }
+        } else if (typeof optionsValue === 'boolean') {
+            if (typeof configValue === 'string') {
+                options[key] = ['1', 'true', 'on'].indexOf(configValue.toLowerCase()) > -1;
+            } else if (typeof configValue === 'boolean') {
+                options[key] = configValue;
+            } else if (typeof configValue === 'number') {
+                options[key] = configValue === 1;
+            } else {
+                options[key] = false;
+            }
+        } else if (typeof optionsValue === 'number') {
+            options[key] = Number(configValue) || 0;
+        } else if (typeof optionsValue === 'object' && typeof configValue === 'object') {
+            mapOptionValues(optionsValue, configValue);
+        }
+    }
+}
 
 @Injectable({
     providedIn: 'root'
 })
-export class ConfigRoot {
+export class ConfigService {
     readonly loadEvent: Observable<ConfigLoadingContext>;
 
     private readonly options: ConfigOptions;
     private readonly loadSubject = new BehaviorSubject<ConfigLoadingContext>({});
-    private readonly fetchRequests: { [key: string]: Observable<ConfigTemplate> } = {};
+    private readonly fetchRequests: { [key: string]: Observable<ConfigSection> } = {};
 
     private loading = false;
     private completed = false;
@@ -53,6 +102,7 @@ export class ConfigRoot {
 
     constructor(
         @Inject(CONFIG_PROVIDER) configProviders: ConfigProvider[],
+        private readonly injector: Injector,
         @Optional() @Inject(CONFIG_OPTIONS) options?: ConfigOptions
     ) {
         if (!configProviders || !configProviders.length) {
@@ -65,15 +115,15 @@ export class ConfigRoot {
         this.loadEvent = this.loadSubject.asObservable();
     }
 
-    load(): Observable<ConfigTemplate[]> {
+    load(): Observable<ConfigSection[]> {
         return this.loadInternal(false);
     }
 
-    reload(): Observable<ConfigTemplate[]> {
+    reload(): Observable<ConfigSection[]> {
         return this.loadInternal(true);
     }
 
-    getValue(key: string): string | null {
+    getValue(key: string): string | ConfigSection | null {
         // const keyArray = key.split(/\.|:/);
         // const result = keyArray.reduce((acc, current: string) => acc && acc[current], this.cachedConfig);
 
@@ -93,11 +143,33 @@ export class ConfigRoot {
         }
     }
 
-    getSection(key: string): ConfigSection | null {
-        return null;
+    getOptions<T extends NewableOptions<T>>(optionsClass: NewableOptions<T>): T {
+        const normalizedKey = this.getNormalizedKey(optionsClass);
+        const configSection = this.getValue(normalizedKey);
+        let optionsObj = this.injector.get<T>(optionsClass);
+        optionsObj = optionsObj || new optionsClass();
+        if (configSection == null || typeof configSection !== 'object') {
+            return optionsObj;
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        mapOptionValues(optionsObj as any, configSection);
+
+        return optionsObj;
     }
 
-    private loadInternal(reload: boolean): Observable<ConfigTemplate[]> {
+    private getNormalizedKey<T extends NewableOptions<T>>(optionsClass: NewableOptions<T>): string {
+        let normalizedKey = optionsClass.name;
+        if (normalizedKey.length > OptionsSuffix.length && normalizedKey.endsWith(OptionsSuffix)) {
+            normalizedKey = normalizedKey.substr(0, normalizedKey.length - OptionsSuffix.length);
+        }
+
+        normalizedKey = normalizedKey[0].toLowerCase() + normalizedKey.substr(1);
+
+        return normalizedKey;
+    }
+
+    private loadInternal(reload: boolean): Observable<ConfigSection[]> {
         if (this.completed && !reload) {
             this.log('Configuration already loaded.');
 
