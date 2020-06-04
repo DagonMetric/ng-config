737a3a89d0fe4fb602cc45ae9c2ed0c46630248f
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-04 11:10:49 +0630

    Update config-provider.ts

--- modules/ng-config/src/config-provider.ts
+++ modules/ng-config/src/config-provider.ts
@@ -6,9 +6,11 @@
  * found under the LICENSE file in the root directory of this source tree.
  */
 
+import { InjectionToken } from '@angular/core';
+
 import { Observable } from 'rxjs';
 
-import { ConfigSection } from './config-section';
+import { ConfigSection } from './config-value';
 
 /**
  * The ConfigProvider interface.
@@ -17,3 +19,5 @@ export interface ConfigProvider {
     readonly name: string;
     load(): Observable<ConfigSection>;
 }
+
+export const CONFIG_PROVIDER = new InjectionToken<ConfigProvider>('ConfigProvider');
