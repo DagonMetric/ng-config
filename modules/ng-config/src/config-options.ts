df15af3160dd2f2c2c8c2351c8e09543e7e7fe7e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-04 11:10:31 +0630

    UPdate config-options.ts

--- modules/ng-config/src/config-options.ts
+++ modules/ng-config/src/config-options.ts
@@ -8,6 +8,10 @@ export interface ConfigOptions {
      * Set true to log debug information.
      */
     trace?: boolean;
+    /**
+     * Options suffix for options class. Default is 'Options'.
+     */
+    optionsSuffix?: string;
 }
 
 export const CONFIG_OPTIONS = new InjectionToken<ConfigOptions>('ConfigOptions');
