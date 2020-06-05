1876b1a06d8660b4d101266218dd2dfa48cab52a
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-05 14:02:09 +0630

    Update HttpConfigProviderOptions

--- modules/ng-config/http-config-provider/src/http-config-provider-options.ts
+++ modules/ng-config/http-config-provider/src/http-config-provider-options.ts
@@ -16,10 +16,6 @@ export interface HttpConfigProviderOptions {
      * The endpoint url string or InjectionToken.
      */
     endpoint: string | InjectionToken<string>;
-    /**
-     * The loader order.
-     */
-    order?: number;
 }
 
 export const HTTP_CONFIG_PROVIDER_OPTIONS = new InjectionToken<HttpConfigProviderOptions>('HttpConfigProviderOptions');
