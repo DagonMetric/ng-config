e79c99d2ef6fed20ceea73865e89a977aa785e1b
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-09 01:15:47 +0630

    Update demo-app

--- samples/demo-app/src/main.ts
+++ samples/demo-app/src/main.ts
@@ -1,13 +1,23 @@
-import { enableProdMode } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+import './polyfills';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-    enableProdMode();
-}
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
+    .then((ref) => {
+        // Ensure Angular destroys itself on hot reloads.
+        if ((window as any).ngRef) {
+            (window as any).ngRef.destroy();
+        }
+
+        (window as any).ngRef = ref;
+
+        // Otherwise, log the boot error
+    })
     .catch((err) => console.error(err));
