6fb3c694da5f7d82561369948aaa8cf8b267f236
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-09 10:12:12 +0630

    Update

--- samples/demo-app/src/main.ts
+++ samples/demo-app/src/main.ts
@@ -11,13 +11,12 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .then((ref) => {
-        // Ensure Angular destroys itself on hot reloads.
-        if ((window as any).ngRef) {
-            (window as any).ngRef.destroy();
+        if (typeof window !== 'undefined') {
+            // Ensure Angular destroys itself on hot reloads.
+            if ((window as any).ngRef) {
+                (window as any).ngRef.destroy();
+            }
+            (window as any).ngRef = ref;
         }
-
-        (window as any).ngRef = ref;
-
-        // Otherwise, log the boot error
     })
     .catch((err) => console.error(err));
