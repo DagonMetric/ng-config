0994527e6aa812dfbb30c7f32c817255bfe98796
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-15 20:58:09 +0630

    Update e2e tests

--- samples/demo-app-e2e/src/app.e2e-spec.ts
+++ samples/demo-app-e2e/src/app.e2e-spec.ts
@@ -14,6 +14,24 @@ describe('workspace-project App', () => {
         void expect(page.getTitleText()).toEqual('NG-CONFIG Demo');
     });
 
+    it(`should display 'appOptions.name' text`, () => {
+        void page.navigateTo();
+
+        void expect(page.getMapTypeSectionText()).toEqual('ng-config-demo');
+    });
+
+    it(`should display 'childOptions.key1' text`, () => {
+        void page.navigateTo();
+
+        void expect(page.getMapObjectSectionText()).toEqual('a');
+    });
+
+    it(`should display 'configValue' text`, () => {
+        void page.navigateTo();
+
+        void expect(page.getGetValueSectionText()).toEqual('value1');
+    });
+
     afterEach(async () => {
         // Assert that there are no errors emitted from the browser
         const logs = await browser.manage().logs().get(logging.Type.BROWSER);
--- samples/demo-app-e2e/src/app.po.ts
+++ samples/demo-app-e2e/src/app.po.ts
@@ -9,7 +9,15 @@ export class AppPage {
         return element(by.css('app-root header h1')).getText() as Promise<string>;
     }
 
-    getFirstSectionText(): Promise<string> {
-        return element(by.css('app-root section h1')).getText() as Promise<string>;
+    getMapTypeSectionText(): Promise<string> {
+        return element(by.css('app-root section .app-options-name')).getText() as Promise<string>;
+    }
+
+    getMapObjectSectionText(): Promise<string> {
+        return element(by.css('app-root section .child-options-key1')).getText() as Promise<string>;
+    }
+
+    getGetValueSectionText(): Promise<string> {
+        return element(by.css('app-root section .key1')).getText() as Promise<string>;
     }
 }
